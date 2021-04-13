601fee3bc01afd965cabb2a88fdd7fbb85edb530
alosoz <79115868+alosoz@users.noreply.github.com>
2021-04-14 02:34:27 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -8,6 +8,8 @@ public class MyGitClass {
 		System.out.println("Version 2");
 		System.out.println("Version 3");
 		System.out.println("Version uzak sunucudaki konum");
+		System.out.println("Version uzak sunucudaki konum");
+		System.out.println("Version uzak sunucudaki konum");
 	}
 
 }
